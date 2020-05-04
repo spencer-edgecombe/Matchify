d06dac9ed648665904350acf18be76e9953a7f2c
Spencer Edgecombe <edge1190@mylaurier.ca>
2020-05-04 11:25:20 -0400

    Initial Commit

---
+++ Matchify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Matchify
+//
+//  Created by Spencer Edgecombe on 2020-05-04.
+//  Copyright © 2020 com.spenceredgecombe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
